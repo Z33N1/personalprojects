0801744f78eab2ed2fdfa587bca40d3cfa2bbb81
Z33N1 <92804815+Z33N1@users.noreply.github.com>
2025-07-02 14:10:11 -0400

    fixxed reading and making a paste

--- clipcode/src/main.jsx
+++ clipcode/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Create from "./pages/Create.jsx";
+import ViewPaste from "./pages/ViewPaste.jsx"; // 👈 Add this
+
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
@@ -11,6 +13,7 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/create" element={<Create />} />
+        <Route path="/paste/:id" element={<ViewPaste />} /> {/* 👈 ADD THIS */}
       </Routes>
     </BrowserRouter>
   </StrictMode>
